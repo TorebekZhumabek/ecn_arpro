5a5deb530eb113ec264bd0ac77094ee28073b92d
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2017-03-13 17:29:18 +0100

    typos in subject

--- examples/thr.cpp
+++ examples/thr.cpp
@@ -8,16 +8,19 @@
 
 using namespace std;
 
-void Count(int id, int i)
+int Count(int id, int i)
 {
     std::string spaces = "";
     for(int k=0;k<30*(id-1);++k)
         spaces += " ";
+    int c = 1;
     for(unsigned int k=0;k<i;++k)
     {
+        c *= k+1;
         printf("%sfunction: %i, count: %i\n", spaces.c_str(), id, k);
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
+    return c;
 }
 
 
@@ -25,12 +28,12 @@ int main(int argc, char ** argv)
 {
 
     std::cout << "Without threads" << std::endl;
-    Count(1, 10);
-    Count(2, 10);
+    Count(1, 1000);
+    Count(2, 1000);
 
 std::cout << "With threads" << std::endl;
-    std::thread t1(Count, 1, 10);
-    std::thread t2(Count, 2, 10);
+    std::thread t1(Count, 1, 1000);
+    std::thread t2(Count, 2, 1000);
 
     t1.join();
     t2.join();
